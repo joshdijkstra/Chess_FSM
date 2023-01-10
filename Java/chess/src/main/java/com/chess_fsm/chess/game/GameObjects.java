1fda729b8aef71af6620b6135dd1d716a6c338da
Josh Dykstra <josh_dykstra98@hotmail.co.uk>
2023-01-10 14:09:22 +0000

    king legal moves

--- Java/chess/src/main/java/com/chess_fsm/chess/game/GameObjects.java
+++ Java/chess/src/main/java/com/chess_fsm/chess/game/GameObjects.java
@@ -4,15 +4,15 @@ import com.chess_fsm.chess.game.gameLogic.Board.Board;
 
 public class GameObjects {
     private final Board board;
-    private final String startingFen = "rnbqkbnr/1pp1pppp/8/8/8/8/1PPPPPPP/RNBQKBNR w KQkq - 0 1";
-
-
+    private final String startingFen = "rnbqkbnr/1pp1pppp/8/8/6P1/8/1PPPPP1P/RNBQKBNR w KQkq - 0 1";
 
     public GameObjects() {
         this.board = new Board();
         this.board.initPieces(startingFen);
     }
 
-    public Board getBoard() { return board; }
-    
+    public Board getBoard() {
+        return board;
+    }
+
 }
--- Java/chess/src/main/java/com/chess_fsm/chess/game/gameLogic/Pieces/King.java
+++ Java/chess/src/main/java/com/chess_fsm/chess/game/gameLogic/Pieces/King.java
@@ -4,34 +4,37 @@ import com.chess_fsm.chess.game.gameLogic.Board.Board;
 import com.chess_fsm.chess.game.gameLogic.Board.Square;
 
 public class King extends Piece {
-      
-        public King(int x, int y, boolean isWhite) {
-          super(x,y,isWhite, PieceType.KING);
-        }
-      
-        public boolean canMoveTo(int x, int y) {
-          // Kings can move one square in any direction
-          if (Math.abs(x - this.x) > 1 || Math.abs(y - this.y) > 1) {
-            return false;
-          }
-      
-          // Kings cannot move to their current position
-          if (x == this.x && y == this.y) {
-            return false;
-          }
-      
-          return true;
-        }    
-        public void getLegalMoves(Board board){
-          Square[][] squares = board.getSquares();
-          for (int row = 0; row < squares.length; row++) {
-              for (int col = 0; col < squares[row].length; col++) {
-                  if (this.canMoveTo(row,col)){
-                    this.addLegalMove(row, col);
 
-                  }
-              }
-           }
+  public King(int x, int y, boolean isWhite) {
+    super(x, y, isWhite, PieceType.KING);
+  }
+
+  public boolean canMoveTo(int x, int y) {
+    // Kings can move one square in any direction
+    if (Math.abs(x - this.x) > 1 || Math.abs(y - this.y) > 1) {
+      return false;
+    }
+
+    // Kings cannot move to their current position
+    if (x == this.x && y == this.y) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public void getLegalMoves(Board board) {
+    for (int x = -1; x < 2; x++) {
+      for (int y = -1; y < 2; y++) {
+        if ((x != 0 && y != 0) && this.x + x >= 0 && this.x + x < 8 && this.y + y < 8 && this.y + y >= 0) {
+          if (board.isPieceOnSquare(this.x + x, this.y + y) != null
+              && board.isPieceOnSquare(this.x + x, this.y + y).isWhite != this.isWhite) {
+            this.addLegalMove(this.x + x, this.y + y);
+          } else if (board.isPieceOnSquare(this.x + x, this.y + y) == null) {
+            this.addLegalMove(this.x + x, this.y + y);
+          }
+        }
       }
-      }
-      
+    }
+  }
+}
