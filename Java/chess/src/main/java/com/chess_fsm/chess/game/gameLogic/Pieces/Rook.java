1a316d46dc07a04c8b77e60d4bbaa72c59e44b29
Josh Dykstra <josh_dykstra98@hotmail.co.uk>
2023-01-19 12:53:34 +0000

    refactored rook

--- Java/chess/src/main/java/com/chess_fsm/chess/game/gameLogic/Pieces/Rook.java
+++ Java/chess/src/main/java/com/chess_fsm/chess/game/gameLogic/Pieces/Rook.java
@@ -15,53 +15,32 @@ public class Rook extends Piece {
   public void getLegalMoves(Board board) {
     this.clearLegalMoves();
     this.clearDefenders();
-    for (int row = this.x + 1; row < 8; row++) {
-      if (board.isPieceOnSquare(row, this.y) == null) {
-        this.addLegalMove(row, this.y, board);
-      } else if (board.isPieceOnSquare(row, this.y).isWhite != this.isWhite) {
-        this.addLegalMove(row, this.y, board);
-        break;
-      } else if (board.isPieceOnSquare(row, this.y).isWhite == this.isWhite) {
-        this.addDefends(row, this.y);
-        break;
+
+    for (int inc : new int[] { 1, -1 }) {
+      for (int row = this.x + inc; row < 8 && row >= 0; row += inc) {
+        if (board.isPieceOnSquare(row, this.y) == null) {
+          this.addLegalMove(row, this.y, board);
+        } else if (board.isPieceOnSquare(row, this.y).isWhite != this.isWhite) {
+          this.addLegalMove(row, this.y, board);
+          break;
+        } else if (board.isPieceOnSquare(row, this.y).isWhite == this.isWhite) {
+          this.addDefends(row, this.y);
+          break;
+        }
       }
     }
 
-    for (int row = this.x - 1; row >= 0; row--) {
-      if (board.isPieceOnSquare(row, this.y) == null) {
-        this.addLegalMove(row, this.y, board);
-      } else if (board.isPieceOnSquare(row, this.y).isWhite != this.isWhite) {
-        this.addLegalMove(row, this.y, board);
-        break;
-      } else if (board.isPieceOnSquare(row, this.y).isWhite == this.isWhite) {
-        this.addDefends(row, this.y);
-        break;
-      }
-    }
-
-    for (int col = this.y + 1; col < 8; col++) {
-      if (board.isPieceOnSquare(this.x, col) == null) {
-        this.addLegalMove(this.x, col, board);
-      } else if (board.isPieceOnSquare(this.x, col).isWhite != this.isWhite) {
-        this.addLegalMove(this.x, col, board);
-        break;
-      } else if (board.isPieceOnSquare(this.x, col).isWhite == this.isWhite) {
-        this.addDefends(this.x, col);
-
-        break;
-      }
-    }
-
-    for (int col = this.y - 1; col >= 0; col--) {
-      if (board.isPieceOnSquare(this.x, col) == null) {
-        this.addLegalMove(this.x, col, board);
-      } else if (board.isPieceOnSquare(this.x, col).isWhite != this.isWhite) {
-        this.addLegalMove(this.x, col, board);
-        break;
-      } else if (board.isPieceOnSquare(this.x, col).isWhite == this.isWhite) {
-        this.addDefends(this.x, col);
-
-        break;
+    for (int inc : new int[] { 1, -1 }) {
+      for (int col = this.y + inc; col < 8 && col >= 0; col += inc) {
+        if (board.isPieceOnSquare(this.x, col) == null) {
+          this.addLegalMove(this.x, col, board);
+        } else if (board.isPieceOnSquare(this.x, col).isWhite != this.isWhite) {
+          this.addLegalMove(this.x, col, board);
+          break;
+        } else if (board.isPieceOnSquare(this.x, col).isWhite == this.isWhite) {
+          this.addDefends(this.x, col);
+          break;
+        }
       }
     }
   }
